6dbffdd34c8d02fa7e22648ced999ed1bf8996da
Adrian Alfred C. Bonpin <adrianbonpin@gmail.com>
2024-03-05 20:56:27 +0800

    feat: add framer-motion, contact us honeypot for bots

--- components/modules/team.tsx
+++ components/modules/team.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import styles from './team.module.css'
 import Link from 'next/link'
 
+import { motion } from 'framer-motion'
+
 export type members = {
     name: string
     img: string
@@ -14,7 +16,14 @@ export default function Members({ members }: { members: members }) {
     return (
         <>
             {members.map((data, index) => (
-                <div key={index} className={styles.member}>
+                <motion.div
+                    key={index}
+                    className={styles.member}
+                    initial={{ opacity: '0%' }}
+                    whileInView={{ opacity: '100%' }}
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.6 + (0.2 * index) }}
+                >
                     <Image src={`/team/${data.img}`} width={450} height={450} alt={`picture of ${data.name}`} />
                     <h5>{data.name}</h5>
                     <h5>{data.role}</h5>
@@ -24,7 +33,7 @@ export default function Members({ members }: { members: members }) {
                         </span>
                     )}
                     <p>{data.description}</p>
-                </div>
+                </motion.div>
             ))}
         </>
     )
