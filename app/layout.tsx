fbf68056de3bbb084386b0113296ed42ee2d1ad2
Adrian Bonpin <adrianbonpin@gmail.com>
2024-09-04 10:25:43 +0800

    fix: jsonlb

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import consMeta from '@/utils/metadata'
 import Navbar, { NavLink } from '@/components/navbar'
 import { Footer } from '@/components/footer'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { OrganizationJsonLd } from 'next-seo'
 
 export const metadata: Metadata = consMeta()
 
@@ -14,10 +15,23 @@ const mont = Montserrat({ subsets: ['latin'] })
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" className="overflow-x-clip bg-background">
-            <head>
-                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
-            </head>
             <body className={mont.className + ' relative overflow-x-clip bg-background text-text'}>
+                <OrganizationJsonLd
+                    useAppDir={true}
+                    type="Corporation"
+                    logo="https://devgo.studio/icon.webp"
+                    legalName="DEVGO Studio"
+                    name="DEVGO Studio"
+                    url="https://devgo.studio"
+                    contactPoint={[
+                        {
+                            email: 'official@devgo.studio',
+                            areaServed: 'Philippines',
+                            availableLanguage: ['English', 'Filipino'],
+                        },
+                    ]}
+                    sameAs={['https://www.devgo.studio']}
+                />
                 <Navbar>
                     <NavLink href="/#about">About Us</NavLink>
                     <NavLink href="/#services">Services</NavLink>
@@ -30,25 +44,4 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             </body>
         </html>
     )
-}
-
-const jsonLd = {
-    '@context': 'https://schema.org',
-    '@type': 'WebSite',
-    url: 'https://devgo.studio',
-    mainEntityOfPage: {
-        '@type': 'WebPage',
-        '@id': 'https://devgo.studio',
-    },
-    headline: 'DEVGO Studio, build your dreams to reality.',
-    description: 'A web development studio based in Cebu. Helping you build your dreams to reality.',
-    inLanguage: 'en-US',
-    isFamilyFriendly: 'true',
-    sitelinks: [
-        {
-            '@type': 'WebPageElement',
-            name: 'Contact Us',
-            url: 'https://devgo.studio/#contact',
-        },
-    ],
 }
