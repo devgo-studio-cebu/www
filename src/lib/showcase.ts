a7606f7ff9573866053bf541766ebe21b5df493c
Adrian Bonpin <adrianbonpin@gmail.com>
2026-07-09 20:45:17 +0800

    Add 4 new projects to showcase: Isla Kalayo, Noteworthy, 8GPI, RDMD Studio

--- src/lib/showcase.ts
+++ src/lib/showcase.ts
@@ -2,6 +2,10 @@ import hopethreadsImg from '../assets/works/hopethreads.png'
 import palmsImg from '../assets/works/palms-agency-global.png'
 import serialkittenImg from '../assets/works/serialkitten.png'
 import wildRoundsImg from '../assets/works/wild-rounds-pilipinas-open.png'
+import islaKalayoImg from '../assets/works/isla-kalayo.png'
+import noteworthyImg from '../assets/works/noteworthy.png'
+import gpiImg from '../assets/works/8gpi.png'
+import rdmdStudioImg from '../assets/works/rdmd-studio.png'
 
 export interface Project {
     title: string
@@ -15,6 +19,12 @@ export interface Project {
 // manifest for showcase projects.
 export const projects: Project[] = [
     {
+        title: "Isla Kalayo",
+        image: islaKalayoImg,
+        url: "https://islakalayo.com",
+        year: 2026,
+    },
+    {
         title: "Wild Rounds Pilipinas Open",
         image: wildRoundsImg,
         url: "https://wildroundspilipinasopen.com",
@@ -27,10 +37,28 @@ export const projects: Project[] = [
         year: 2026,
     },
     {
+        title: "8GPI",
+        image: gpiImg,
+        url: "https://8gpi.com",
+        year: 2025,
+    },
+    {
+        title: "RDMD Studio",
+        image: rdmdStudioImg,
+        url: "https://rdmdstudio.com",
+        year: 2025,
+    },
+    {
         title: "Palms Agency Global",
         image: palmsImg,
         url: "https://palms-agency-global.com",
         year: 2025,
+    },
+    {
+        title: "Noteworthy",
+        image: noteworthyImg,
+        url: "https://noteworthy-va.com",
+        year: 2024,
     },
     {
         title: "Serial Kitten",
